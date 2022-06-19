122caeec9d51a9e87785dbe372f4bda1844f88b1
Darwin Rafael Rafael <102385593+Darwin111233@users.noreply.github.com>
2022-06-19 05:43:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#Nombre: Darwin Rafael Castro Garcia
+#Grupo: 7
+
+Porcentaje_Po_Culiacan = 0
+
+Porcentaje_Po_Mazatlan = 0
+
+Poblacion_Mujeres = 1532128
+
+Poblacion_Hombres = 1494815
+
+Temperatura_Media = 25.45
+
+Poblacion_Total = 0.0
+
+Clima_Calido = "cálido"
+
+Clima_Subhumedo = "subhúmedo"
+
+Clima_Seco = "seco"
+
+Clima_Semiseco = "semiseco"
+
+Poblacion_Total= Poblacion_Hombres + Poblacion_Mujeres
+
+print("La poblacion Total del Estado de Sinaloa es: ", +Poblacion_Total)
+Poblacion_Total = Poblacion_Total *33.15
+
+Porcentaje_Po_Culiacan = Poblacion_Total / 100
+
+print("La Poblacion de la ciudad de Culiacan es: ",+Porcentaje_Po_Culiacan)
+
+Poblacion_Total= Poblacion_Hombres + Poblacion_Mujeres
+
+Poblacion_Total = Poblacion_Total *16.57
+
+Porcentaje_Po_Mazatlan = Poblacion_Total/100
+
+print("La Poblacion de la ciudad de mazatlan es: ",+Porcentaje_Po_Mazatlan,)
+
+print("Ademas el estado de sinaloa cuenta con una temperatura media anual de: ",Temperatura_Media,"grados Con el cual cuenta con varios tipos de climas que son: ",Clima_Calido,", ",Clima_Subhumedo,", ",Clima_Seco," Y ",Clima_Semiseco)
